81fbeb381e8aaa8bd38171a685a9b127a6e2cfc1
Lukas Bestle <lukas@lu-x.me>
2013-07-02 16:55:13 +0200

    Get custom app paths from a special paths.php file outside the repo

--- lib/core.php
+++ lib/core.php
@@ -13,18 +13,23 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
 
+// Check for a custom path configuration file
+if(file_exists(__DIR__ . '/../../paths.php')) {
+	include __DIR__ . '/../../paths.php';
+}
+
 // Roots
-define('ROOT',            realpath(__DIR__ . '/..'));
-define('ROOT_LIB',        realpath(ROOT . '/lib'));
-define('ROOT_THIRDPARTY', realpath(ROOT_LIB . '/thirdparty'));
-define('ROOT_CORE',       realpath(ROOT_LIB . '/core'));
-define('ROOT_ENDPOINTS',  realpath(ROOT_LIB . '/endpoints'));
-define('ROOT_TASKS',      realpath(ROOT_ENDPOINTS . '/tasks'));
-define('ROOT_TEMPLATES',  realpath(ROOT . '/templates'));
-define('ROOT_SNIPPETS',   realpath(ROOT_TEMPLATES . '/snippets'));
-define('ROOT_FILES',      realpath(ROOT . '/files'));
-define('ROOT_CACHE',      realpath(ROOT . '/cache'));
-define('ROOT_LOGS',       realpath(ROOT . '/logs'));
+if(!defined('ROOT'))            define('ROOT',            realpath(__DIR__ . '/..'));
+if(!defined('ROOT_LIB'))        define('ROOT_LIB',        realpath(ROOT . '/lib'));
+if(!defined('ROOT_THIRDPARTY')) define('ROOT_THIRDPARTY', realpath(ROOT_LIB . '/thirdparty'));
+if(!defined('ROOT_CORE'))       define('ROOT_CORE',       realpath(ROOT_LIB . '/core'));
+if(!defined('ROOT_ENDPOINTS'))  define('ROOT_ENDPOINTS',  realpath(ROOT_LIB . '/endpoints'));
+if(!defined('ROOT_TASKS'))      define('ROOT_TASKS',      realpath(ROOT_ENDPOINTS . '/tasks'));
+if(!defined('ROOT_TEMPLATES'))  define('ROOT_TEMPLATES',  realpath(ROOT . '/templates'));
+if(!defined('ROOT_SNIPPETS'))   define('ROOT_SNIPPETS',   realpath(ROOT_TEMPLATES . '/snippets'));
+if(!defined('ROOT_FILES'))      define('ROOT_FILES',      realpath(ROOT . '/files'));
+if(!defined('ROOT_CACHE'))      define('ROOT_CACHE',      realpath(ROOT . '/cache'));
+if(!defined('ROOT_LOGS'))       define('ROOT_LOGS',       realpath(ROOT . '/logs'));
 
 // Autoload classes
 spl_autoload_register(function($class) {
